938439abd6079b73b06431f2901cb08b4ca62148
bruno <brunoginfosec@proton.me>
2023-12-11 16:26:18 -0300

    created functions to get data from profile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,24 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+    const title = document.getElementById('profile.name')
+    title.innerText = profileData.name
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `tel:${profileData.phone}`
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}`
+}
+
+
 (async () => {
     const profileData = await fetchProfileData()
-    console.log(profileData)
+    updateProfileInfo(profileData)
+    // console.log(profileData)
 })()
